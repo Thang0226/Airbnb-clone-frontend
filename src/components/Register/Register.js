4c07e3dff20555b51a92e46b81bf9ca799a1ace7
Thang0226 <thang.nd0226@gmail.com>
2025-02-12 23:24:30 +0700

    add form validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,56 +25,52 @@ import {
 import {TbBrandAirbnb} from "react-icons/tb";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
+import {useRef} from "react";
+import {Formik} from "formik";
+import * as Yup from "yup";
 
 export default function Register() {
-
+    const navigate = useNavigate();
     const [visible, setVisible] = useState(false);
     const REGEX = {
-        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/,
-        password: /^[a-zA-Z0-9!@#$%^&*)(+=._-]{6,}$/,
+        username: /^[a-zA-Z0-9_]{4,30}$/,
+        password: /^[a-zA-Z0-9!@#$%^&*)(+=._-]{6,32}$/,
         phone: /^0[0-9]{9}$/
     };
-    const [form, setForm] = useState({});
-    const navigate = useNavigate();
 
-    const handleChange = (event) => {
-        setForm({
-            ...form,
-            [event.target.name]: event.target.value
-        });
-    }
+    const initialValues = {
+        username: '',
+        phone: '',
+        password: '',
+        confirm_password: ''
+    };
+    const formikRef = useRef(null);
 
-    function handleSubmit() {
-        alert("Login in successfully!");
-        navigate("/login", {state: {account: form.username}});
-    }
+    const validationSchema = Yup.object().shape({
+        username: Yup.string()
+            .min(4, "At least 4 characters long")
+            .max(30, "At most 30 characters long")
+            .matches(REGEX.username, "Not contain special characters"),
+        phone: Yup.string().matches(REGEX.phone, "Invalid phone number"),
+        password: Yup.string()
+            .min(6, "At least 6 characters long")
+            .max(32, "At most 32 characters long")
+            .matches(REGEX.password, "Invalid password"),
+        confirm_password: Yup.string()
+            .test(
+                "Same passwords",
+                "Passwords must match",
+                function (value) {
+                    return this.parent.password === value;
+                }
+            ),
+    });
 
-    function handleValidate() {
-        const errors = {};
-
-        if (form.username) {
-            if (!REGEX.username.test(form.username)) {
-                errors.username = "Invalid username";
-            }
-        }
-
-        if (form.phone) {
-            if (!REGEX.phone.test(form.phone)) {
-                errors.phone = "Invalid phone number";
-            }
-        }
-
-        if (form.password) {
-            if (!REGEX.password.test(form.password)) {
-                errors.password = "Invalid password";
-            }
-            if (form.password !== form.confirm_password) {
-                errors.confirm_password = "Confirm password not match";
-            }
-        }
-
-        return errors;
+    const handleSubmit = () => {
+        console.log(formikRef.current.values);
+        alert("Register successfully!");
+        navigate("/login");
     }
 
     return (
@@ -118,44 +114,60 @@ export default function Register() {
                 </CContainer>
             </CNavbar>
             <h2 className={styles.title}>Register New Account</h2>
-            <CForm className={styles.formBox}>
-                <CRow className="mb-3">
-                    <CFormLabel htmlFor="username" className="col-sm-4 col-form-label">
-                        Username:
-                    </CFormLabel>
-                    <CCol sm={8}>
-                        <CFormInput type="text" id="username" name="username" onChange={handleChange}/>
-                    </CCol>
-                </CRow>
-                <CRow className="mb-3">
-                    <CFormLabel htmlFor="phone" className="col-sm-4 col-form-label">
-                        Phone:
-                    </CFormLabel>
-                    <CCol sm={8}>
-                        <CFormInput type="text" id="phone" name="phone" onChange={handleChange}/>
-                    </CCol>
-                </CRow>
-                <CRow className="mb-3">
-                    <CFormLabel htmlFor="password" className="col-sm-4 col-form-label">
-                        Password:
-                    </CFormLabel>
-                    <CCol sm={8}>
-                        <CFormInput type="password" id="password" name="password" onChange={handleChange}/>
-                    </CCol>
-                </CRow>
-                <CRow className="mb-4">
-                    <CFormLabel htmlFor="confirm_password" className="col-sm-4 col-form-label">
-                        Confirm Password:
-                    </CFormLabel>
-                    <CCol sm={8}>
-                        <CFormInput type="password" id="confirm_password" name="confirm_password"
-                                    onChange={handleChange}/>
-                    </CCol>
-                </CRow>
-                <CButton color="primary" type="submit">
-                    Register
-                </CButton>
-            </CForm>
+            <Formik initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                    innerRef={formikRef}>
+                {({errors, touched, handleChange, handleSubmit}) => (
+                    <CForm className={styles.formBox} onSubmit={handleSubmit}>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="username" className="col-sm-4 col-form-label">
+                                Username:
+                            </CFormLabel>
+                            <CCol sm={8}>
+                                <CFormInput type="text" placeholder="user_name123" id="username" name="username"
+                                            onChange={handleChange} required/>
+                                {touched.username && errors.username && <p className="error">{errors.username}</p>}
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="phone" className="col-sm-4 col-form-label">
+                                Phone:
+                            </CFormLabel>
+                            <CCol sm={8}>
+                                <CFormInput type="text" placeholder="0123456789" id="phone" name="phone"
+                                            onChange={handleChange}
+                                            required/>
+                                {touched.phone && errors.phone && <p className="error">{errors.phone}</p>}
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CFormLabel htmlFor="password" className="col-sm-4 col-form-label">
+                                Password:
+                            </CFormLabel>
+                            <CCol sm={8}>
+                                <CFormInput type="password" id="password" name="password" onChange={handleChange}
+                                            required/>
+                                {touched.password && errors.password && <p className="error">{errors.password}</p>}
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-4">
+                            <CFormLabel htmlFor="confirm_password" className="col-sm-4 col-form-label">
+                                Confirm Password:
+                            </CFormLabel>
+                            <CCol sm={8}>
+                                <CFormInput type="password" id="confirm_password" name="confirm_password"
+                                            onChange={handleChange} required/>
+                                {touched.confirm_password && errors.confirm_password &&
+                                    <p className="error">{errors.confirm_password}</p>}
+                            </CCol>
+                        </CRow>
+                        <CButton color="primary" type="submit">
+                            Register
+                        </CButton>
+                    </CForm>)
+                }
+            </Formik>
         </>
     );
 }
