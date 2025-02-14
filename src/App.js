25e006961a59d953932e5f8f1532ad787adb4410
Thang0226 <thang.nd0226@gmail.com>
2025-02-14 11:30:52 +0700

    clear redundant files

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import {Provider} from 'react-redux';
 import store from "./redux/store"
 import './scss/custom.scss'
 import "./App.css"
-import HouseList from "./components/HouseList";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Nabav from "./components/Nabav"
-import SearchBarForHouseAvailable from "./components/SearchBarForHouseAvailable";
 
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 
-function App() {
+export default function App() {
     return (
         <Provider store={store}>
             <HashRouter>
                 <Routes>
-                    <Route path="/" element={<div>
-                          <Nabav/>
-                          <SearchBarForHouseAvailable/>
-                          <HouseList />
-                          </div>}/>
                     <Route path="/register" element={<Register/>}/>
                     <Route path="/login" element={<Login/>}/>
                 </Routes>
             </HashRouter>
         </Provider>
     );
-
-export default App;
+}
