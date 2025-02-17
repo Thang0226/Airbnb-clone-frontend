339d2c30266b9d0408b71228e9a90868f3ae9cb8
dunglt94 <tuandung9594@gmail.com>
2025-02-17 10:31:43 +0700

    fix wrong codes in index.js cause by last commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
-
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import './scss/custom.scss'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import '@coreui/coreui/dist/css/coreui.min.css'
-import './App.css'
-
-import { ToastContainer } from 'react-toastify'
-import Register from './components/Register/Register'
-import Login from './components/Login/Login'
-import UserProfile from "./components/user-profile/UserProfile";
-import ProfileUpdateForm from "./components/user-profile/ProfileUpdateForm";
-
-export default function App() {
-  return (
-    <Provider store={store}>
-      <ToastContainer position="top-right" pauseOnFocusLoss={false} />
-      <HashRouter>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<UserProfile />}/>
-          <Route path="/profile/edit" element={<ProfileUpdateForm />}/>
-        </Routes>
-      </HashRouter>
-    </Provider>
-  )
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
