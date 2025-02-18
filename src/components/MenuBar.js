16fbbcdd8541d705de5f390d42c1b7d268903d84
dunglt94 <tuandung9594@gmail.com>
2025-02-18 12:39:09 +0700

    improve user dropdown bar UI

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -13,16 +13,27 @@ import { TbBrandAirbnb } from 'react-icons/tb'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
-import { BASE_URL_USER } from '../constants/api'
-import { useState } from 'react'
+import { BASE_URL, BASE_URL_USER } from '../constants/api'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { resetAccount } from '../redux/slices/accountSlice'
+import { fetchUserProfile } from '../redux/slices/userProfileSlice'
 
 export default function MenuBar() {
   const [visible, setVisible] = useState(false)
   const navigate = useNavigate()
   const token = localStorage.getItem('token')
   const dispatch = useDispatch()
+  const username = localStorage.getItem('username')
+
+  useEffect(() => {
+    if (username) {
+      dispatch(fetchUserProfile(username))
+    }
+
+  }, [dispatch, username])
+
+  const { userProfile } = useSelector((state) => state.userProfile)
 
   const handleLogout = async () => {
     await axios.post(
@@ -72,8 +83,32 @@ export default function MenuBar() {
               Search
             </CButton>
           </CForm>
-          <CDropdown variant="dropdown" popper={true}>
-            <CDropdownToggle color="secondary">User</CDropdownToggle>
+          <CDropdown variant="dropdown" popper={true} className="bg-gradient rounded">
+            <CDropdownToggle
+              caret={false}
+              color="white"
+              className="border border-dark d-flex align-items-center gap-2"
+              style={{
+                borderRadius: '50px',
+                height: '48px',
+              }}
+            >
+              <i className="bi bi-list" style={{ fontSize: '1.2rem' }}></i>
+
+              <img
+                src={token && userProfile?.avatar
+                  ? `${BASE_URL}/images/${userProfile.avatar}`
+                  : `${BASE_URL}/images/default.jpg`}
+                alt="avatar"
+                className="border border-dark"
+                style={{
+                  width: '32px',
+                  height: '32px',
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                }}
+              />
+            </CDropdownToggle>
             <CDropdownMenu>
               {token ? (
                 <>
