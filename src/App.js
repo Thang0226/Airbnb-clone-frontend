4e52c84a4134c98a64afa6da7daefe52d79d1c6b
dunglt94 <tuandung9594@gmail.com>
2025-02-20 10:22:56 +0700

    improve admin layout

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Homepage from './components/homepage/Homepage'
 import HostRequests from './components/admin/HostRequests'
 import RequireAuth from './components/auth/RequireAuth'
 import AdminLayout from './components/admin/AdminLayout'
+import Dashboard from './components/admin/Dashboard'
 import ChangePassword from './components/user-change-password/ChangePassword'
 
 
@@ -46,7 +47,9 @@ export default function App() {
           {/* Các route dành riêng cho admin */}
           <Route element={<RequireAuth allowedRoles={['ROLE_ADMIN']} />}>
             <Route element={<AdminLayout />}>
-              <Route path="/admin" element={<HostRequests />} />
+              <Route path="/admin" element={<Dashboard />} />
+              <Route path="/admin/host/request" element={<HostRequests />} />
+              <Route path="/admin/users" element={<HostRequests />} />
               {/* Các route admin khác có thể thêm tại đây */}
             </Route>
           </Route>
--- src/components/admin/AdminLayout.js
+++ src/components/admin/AdminLayout.js
@@ -1,25 +1,35 @@
 import { ToastContainer } from 'react-toastify'
-import MenuBar from '../MenuBar'
 import { CContainer, CFooter } from '@coreui/react'
 import { Outlet } from 'react-router-dom'
+import AdminNavBar from './AdminNavBar'
+import { AdminSideBar } from './AdminSideBar'
 
 
-const AdminLayout = ({ children }) => {
+const AdminLayout = () => {
   return (
-    <div className="d-flex flex-column min-h-screen min-vh-100">
+    <div>
       <ToastContainer position="top-right" pauseOnFocusLoss={false} />
-      {/* Navbar */}
-      <MenuBar />
 
-      {/* Content */}
-      <CContainer className="p-4 overflow-auto flex-1">
-        <Outlet />
-      </CContainer>
+      {/* Sidebar */}
+      <AdminSideBar/>
 
-      {/* Footer */}
-      <CFooter className="primary-bg-subtle p-4 mt-auto justify-content-center">
-        © 2025 AirBnb Clone Application. Made by TTLD team. All rights reserved.
-      </CFooter>
+      <div
+       style={{marginLeft: '63.2px'}}
+       className="d-flex flex-column min-h-screen min-vh-100"
+      >
+        {/* Navbar */}
+        <AdminNavBar />
+
+        {/* Content */}
+        <CContainer className="p-4 overflow-auto flex-1">
+          <Outlet />
+        </CContainer>
+
+        {/* Footer */}
+        <CFooter className="primary-bg-subtle p-4 mt-auto justify-content-center">
+          © 2025 AirBnb Clone Application. Made by TTLD team. All rights reserved.
+        </CFooter>
+      </div>
     </div>
   )
 }
