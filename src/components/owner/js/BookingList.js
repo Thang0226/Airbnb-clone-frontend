9d1a1031b9929c5cafb70fc1f6bcd1dbd54dda29
dunglt94 <tuandung9594@gmail.com>
2025-02-25 23:06:35 +0700

    add search bar for host's bookings

--- src/components/owner/js/BookingList.js
+++ src/components/owner/js/BookingList.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
 import { DisplayLoading } from '../../DisplayLoading'
 import { DisplayError } from '../../DisplayError'
 import {
@@ -8,7 +7,7 @@ import {
   CCard,
   CCardBody,
   CCardHeader,
-  CCol,
+  CCol, CFormInput, CFormSelect,
   CRow,
   CTable, CTableBody, CTableDataCell,
   CTableHead,
@@ -18,45 +17,100 @@ import {
 import { UserPagination } from '../../_fragments/CustomerPagination'
 import { getBookings } from '../../../redux/slices/bookingSlice'
 import CurrencyFormat from '../../_fragments/format/CurrencyFormat'
-import styles from '../css/HoustList.module.css';
+import styles from '../css/HoustList.module.css'
 
 const BookingList = () => {
-  const [page, setPage] = useState(0);
-  const [size] = useState(10);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [page, setPage] = useState(0)
+  const [size] = useState(10)
+  const dispatch = useDispatch()
 
-  const username = localStorage.getItem("username")
+  const [houseName, setHouseName] = useState('')
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+  const [status, setStatus] = useState('')
+
+  const username = localStorage.getItem('username')
 
   useEffect(() => {
-    document.title = 'Airbnb | Booking List';
+    document.title = 'Airbnb | Booking List'
   }, [])
 
   useEffect(() => {
-    dispatch(getBookings({username, page, size}));
-  }, [dispatch,page,size, username]);
+    dispatch(getBookings({ username, page, size }))
+  }, [dispatch, page, size, username])
 
-  const { bookings, error, loading, totalPages } = useSelector((state) => state.booking);
+  const { bookings, error, loading, totalPages } = useSelector((state) => state.booking)
 
 
   if (loading || !bookings) return (
-    <DisplayLoading/>
+    <DisplayLoading />
   )
   if (error) return (
     <DisplayError error={error} />
   )
 
   return (
+
     <div className="container">
-      <div className="d-flex align-items-center">
-        <span
-          style={{ cursor: 'pointer', textDecoration: 'underline', color: '#0d6efd' }}
-          onClick={() => navigate("/admin")}
-        >
-          Dashboard
-        </span>
-        <span className="mx-1">{'/'}</span>
-        <span>User List</span>
+      <div>
+        <CRow className="justify-content-center">
+          <CCol sm={12} md={8} lg={3} className="mt-2">
+            <div className="d-flex flex-column flex-grow-1">
+              <label className="fw-bolder">Address</label>
+              <CFormInput
+                type="text"
+                value={houseName}
+                onChange={(e) => setHouseName(e.target.value)}
+                className="ps-2 pt-2"
+                placeholder="Enter house name"
+              />
+            </div>
+          </CCol>
+          <CCol sm={12} md={4} lg={2} className="mt-2">
+            <div className="d-flex flex-column flex-grow-1">
+              <label className="fw-bolder">Status</label>
+              <CFormSelect value={status} onChange={(e) => setStatus(e.target.value)}>
+                <option value="">Chose Status</option>
+                <option key="WAITING" value="WAITING">WAITING</option>
+                <option key="CHECKED_IN" value="CHECKED_IN">CHECKED IN</option>
+                <option key="CHECKED_OUT" value="CHECKED_OUT">CHECKED OUT</option>
+                <option key="CANCELED" value="CANCELED">CANCELED</option>
+              </CFormSelect>
+            </div>
+          </CCol>
+          <CCol sm={12} md={5} lg={3} className="mt-2">
+            <div className="d-flex flex-column flex-grow-1">
+              <label className="fw-bolder">Start Date</label>
+              <CFormInput
+                type="date"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+                className="ps-2 pt-2"
+              />
+            </div>
+          </CCol>
+          <CCol sm={12} md={5} lg={3} className="mt-2">
+            <div className="d-flex flex-column flex-grow-1">
+              <label className="fw-bolder">End Date</label>
+              <CFormInput
+                type="date"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+                className="ps-2 pt-2"
+              />
+            </div>
+          </CCol>
+          <CCol sm={12} md={2} lg={1} className="text-center mt-2">
+            <div style={{minHeight: "24px"}}></div>
+            <CButton
+              color="primary"
+              className="w-100"
+              // onClick={handleSearchButtonClick}
+            >
+              Search
+            </CButton>
+          </CCol>
+        </CRow>
       </div>
       <CRow
         xs={{ cols: 1 }} md={{ cols: 1 }} lg={{ cols: 1 }}
@@ -68,69 +122,57 @@ const BookingList = () => {
               <h4 className="my-3">Booking List</h4>
             </CCardHeader>
             <CCardBody>
-              <CTable hover responsive>
-                <CTableHead>
-                  <CTableRow>
-                    <CTableHeaderCell>House Name</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Start Date</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">End Date</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Rental Days</CTableHeaderCell>
-                    <CTableHeaderCell>Customer Name</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Total cost</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Status</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Action</CTableHeaderCell>
-                  </CTableRow>
-                </CTableHead>
-                <CTableBody>
-                  {bookings.map((booking) => (
-                    <CTableRow key={booking.id} className='align-middle'>
-                      <CTableDataCell
-                        className={styles["house-name"]}
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        title={booking.houseName}
-                      >
-                        {booking.houseName}
-                      </CTableDataCell>
-                      <CTableDataCell className="text-center">
-                        {new Date(booking.startDate).toLocaleDateString('vi-VN', { day: '2-digit', month: '2-digit', year: 'numeric' })}
-                      </CTableDataCell>
-                      <CTableDataCell className="text-center">
-                        {new Date(booking.endDate).toLocaleDateString('vi-VN', { day: '2-digit', month: '2-digit', year: 'numeric' })}
-                      </CTableDataCell>
-                      <CTableDataCell className="text-center">{booking.rentalDay}</CTableDataCell>
-                      <CTableDataCell>{booking.customerName}</CTableDataCell>
-                      <CTableDataCell className="text-end">
-                        <CurrencyFormat value={booking.totalCost} />
-                      </CTableDataCell>
-                      <CTableDataCell className="text-center"> {booking.status.replace("_", " ")}</CTableDataCell>
-                      <CTableDataCell
-                        className="text-center"
-                      >
-                        <CButton
-                          size="sm"
-                          color={"success"}
-                          className="text-white"
-                          style={{ width: "90px" }}
-                          // onClick={() =>}
-                        >
-                          Check In
-                        </CButton>
-                        <CButton
-                          size="sm"
-                          color="primary"
-                          className="text-white ms-2"
-                          style={{ width: "90px" }}
-                          // onClick={() => }
-                        >
-                          Check Out
-                        </CButton>
-                      </CTableDataCell>
+              {!bookings || bookings.length === 0 ? (
+                <div className="text-center py-4">
+                  <h2>No bookings found.</h2>
+                </div>
+              ) : (
+                <CTable hover responsive>
+                  <CTableHead>
+                    <CTableRow>
+                      <CTableHeaderCell>House Name</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Start Date</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">End Date</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Rental Days</CTableHeaderCell>
+                      <CTableHeaderCell>Customer Name</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Total cost</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Status</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Action</CTableHeaderCell>
                     </CTableRow>
-                  ))}
-                </CTableBody>
-              </CTable>
-              <UserPagination page={page} totalPages={totalPages} setPage={setPage} />
+                  </CTableHead>
+                  <CTableBody>
+                    {bookings.map((booking) => (
+                      <CTableRow key={booking.id} className="align-middle">
+                        <CTableDataCell className={styles['house-name']} title={booking.houseName}>
+                          {booking.houseName}
+                        </CTableDataCell>
+                        <CTableDataCell className="text-center">
+                          {new Date(booking.startDate).toLocaleDateString('vi-VN')}
+                        </CTableDataCell>
+                        <CTableDataCell className="text-center">
+                          {new Date(booking.endDate).toLocaleDateString('vi-VN')}
+                        </CTableDataCell>
+                        <CTableDataCell className="text-center">{booking.rentalDay}</CTableDataCell>
+                        <CTableDataCell>{booking.customerName}</CTableDataCell>
+                        <CTableDataCell className="text-end">
+                          <CurrencyFormat value={booking.totalCost} />
+                        </CTableDataCell>
+                        <CTableDataCell className="text-center">{booking.status.replace('_', ' ')}</CTableDataCell>
+                        <CTableDataCell className="text-center">
+                          <CButton size="sm" color="success" className="text-white" style={{ width: '90px' }}>
+                            Check In
+                          </CButton>
+                          <CButton size="sm" color="primary" className="text-white ms-2" style={{ width: '90px' }}>
+                            Check Out
+                          </CButton>
+                        </CTableDataCell>
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                </CTable>
+              )}
+              {bookings && bookings.length > 0 &&
+                <UserPagination page={page} totalPages={totalPages} setPage={setPage} />}
             </CCardBody>
           </CCard>
         </CCol>
