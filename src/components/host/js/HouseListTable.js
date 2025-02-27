8f7de44cdcd994a68f4a15c7dff1894964018b51
dunglt94 <tuandung9594@gmail.com>
2025-02-28 01:29:38 +0700

    add code and successfully call api to update house status and create maintenance record

---
+++ src/components/host/js/HouseListTable.js
@@ -0,0 +1,328 @@
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CContainer, CFormInput, CFormSelect, CModal, CModalBody, CModalFooter, CModalHeader, CRow,
+  CTable, CTableBody, CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import styles from '../css/HoustList.module.css'
+import CurrencyFormat from '../../_fragments/format/CurrencyFormat'
+import { UserPagination } from '../../_fragments/CustomerPagination'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  creatMaintenanceRecord,
+  getBookedDates,
+  getHouseList,
+  getLatestAvailableDate,
+  searchHouses,
+  updateHouseStatus,
+} from '../../../redux/slices/houseSlice'
+import { DisplayLoading } from '../../DisplayLoading'
+import { DisplayError } from '../../DisplayError'
+import { toast } from 'react-toastify'
+import DatePicker from 'react-datepicker'
+import dayjs from 'dayjs'
+
+const HouseListTable = () => {
+  const [page, setPage] = useState(0)
+  const [size] = useState(10)
+  const dispatch = useDispatch()
+  const username = localStorage.getItem('username')
+
+  const [houseName, setHouseName] = useState('')
+  const [status, setStatus] = useState('')
+
+  const today = dayjs().toDate()
+  const [selectedHouse, setSelectedHouse] = useState({})
+  const [newStatus, setNewStatus] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+  const [bookedDates, setBookedDates] = useState([])
+  const [maxAvailableDate, setMaxAvailableDate] = useState(today.getFullYear() + 10)
+  const [minAvailableDate, setMinAvailableDate] = useState(today)
+  const [startDate, setStartDate] = useState(today)
+  const [endDate, setEndDate] = useState(today)
+
+
+  useEffect(() => {
+    document.title = 'Airbnb | House List'
+  }, [])
+
+  useEffect(() => {
+    dispatch(getHouseList({ username, page, size }))
+  }, [dispatch, page, size, username])
+
+  const handleSearch = () => {
+    dispatch(searchHouses({ username, houseName, status, page, size }))
+  }
+
+
+  const handleUpdateStatus = async (house) => {
+    if (house.status === 'RENTED') {
+      toast.warning('Cannot change status of the house being rented!')
+      return
+    }
+    setSelectedHouse(house)
+    setNewStatus(house.status)
+    setModalVisible(true)
+    const houseId = house.id
+    console.log(newStatus)
+    try {
+      const response = await dispatch(getBookedDates({ houseId })).unwrap()
+      console.log(response)
+      if (response.length > 0) {
+        const bookedDatesList = response.map(bookingDTO => ({
+          start: dayjs(new Date(bookingDTO.startDate)).subtract(1, 'day').toDate(),
+          end: new Date(bookingDTO.endDate),
+        }))
+        let newMinDate = new Date() // Lấy ngày hiện tại
+        for (let i = 0; i < bookedDatesList.length; i++) {
+          if (bookedDatesList[i].start <= newMinDate && newMinDate <= bookedDatesList[i].end) {
+            // Nếu hôm nay nằm trong khoảng đặt chỗ, tìm ngày có thể đặt tiếp theo
+            newMinDate = dayjs(bookedDatesList[i].end).add(1, 'day').toDate()
+            break
+          }
+        }
+        console.log(newMinDate)
+        console.log(bookedDatesList)
+
+        setMinAvailableDate(newMinDate)
+        setStartDate(newMinDate)
+        setEndDate(newMinDate)
+        setBookedDates(bookedDatesList)
+      }
+    } catch (error) {
+      console.error('Error fetching booked dates:', error)
+      toast.error('Failed to fetch booked dates')
+    }
+
+    try {
+      const response = await dispatch(getLatestAvailableDate({ houseId, startDate })).unwrap()
+      setMaxAvailableDate(dayjs(response).toDate())
+      console.log(response)
+    } catch (error) {
+      console.error('Error fetching booked dates:', error)
+      toast.error('Failed to fetch booked dates')
+    }
+
+  }
+
+  const handleCloseModal = () => {
+    setModalVisible(false)
+    setNewStatus('')
+    setStartDate(today)
+    setEndDate(today)
+    setBookedDates([])
+  }
+
+  const handleSubmitUpdate = async () => {
+    if (!newStatus) {
+      toast.warning('Please select a status!')
+      return
+    }
+
+    if (newStatus === selectedHouse.status) {
+      handleCloseModal()
+      return
+    }
+
+    if (newStatus === 'MAINTAINING' && (!startDate || !endDate)) {
+      toast.warning('Please select a valid date range for maintenance!')
+      return
+    }
+
+    let previousStatus = selectedHouse.status;
+
+    try {
+      // Cập nhật trạng thái nhà trước
+      const response = await dispatch(updateHouseStatus({ houseId: selectedHouse.id, status: newStatus })).unwrap()
+      console.log(response)
+      toast.success('House status updated successfully!')
+
+      const formattedStartDate = startDate.toISOString().split('T')[0];
+      const formattedEndDate = endDate.toISOString().split('T')[0];
+
+      // Nếu trạng thái được cập nhật thành MAINTAINING, mới tạo maintenance record
+      if (newStatus === 'MAINTAINING') {
+        try {
+          const maintenanceResponse = await dispatch(
+            creatMaintenanceRecord({ houseId: selectedHouse.id, startDate: formattedStartDate, endDate: formattedEndDate })
+          ).unwrap();
+          console.log(maintenanceResponse);
+          toast.success('Maintenance record created successfully!');
+        } catch (error) {
+          // Nếu tạo maintenance record thất bại, rollback trạng thái nhà về trạng thái cũ
+          toast.error('Failed to create maintenance record. Rolling back status...');
+          await dispatch(updateHouseStatus({ houseId: selectedHouse.id, status: previousStatus })).unwrap();
+        }
+      }
+
+      // Đóng modal & cập nhật danh sách
+      handleCloseModal()
+      dispatch(getHouseList({ username, page, size }))
+    } catch (error) {
+      toast.error('Failed to update house status or create maintenance record!')
+    }
+  }
+
+  const { houseList, error, loading, totalPages } = useSelector((state) => state.houses)
+
+  if (loading || !houseList) return (
+    <DisplayLoading />
+  )
+  if (error) return (
+    <DisplayError error={error} />
+  )
+
+  return (
+    <CContainer>
+      <CRow className="justify-content-center">
+        <CCol sm={12} md={8} lg={4} className="mt-2">
+          <div className="d-flex flex-column flex-grow-1">
+            <label className="fw-bolder ps-1">Address</label>
+            <CFormInput
+              type="text"
+              value={houseName}
+              onChange={(e) => setHouseName(e.target.value)}
+              className="ps-2 pt-2"
+              placeholder="Enter house name"
+            />
+          </div>
+        </CCol>
+        <CCol sm={8} md={2} lg={2} className="mt-2">
+          <div className="d-flex flex-column flex-grow-1">
+            <label className="fw-bolder ps-1">Status</label>
+            <CFormSelect value={status} onChange={(e) => setStatus(e.target.value)}>
+              <option value="">Choose Status</option>
+              <option value="AVAILABLE">AVAILABLE</option>
+              <option value="RENTED">RENTED</option>
+              <option value="MAINTAINING">CANCELED</option>
+            </CFormSelect>
+          </div>
+        </CCol>
+        <CCol sm={4} md={2} lg={1} className="text-center mt-2">
+          <div style={{ minHeight: '24px' }}></div>
+          <CButton
+            color="primary"
+            className="w-auto"
+            onClick={handleSearch}
+          >
+            Search
+          </CButton>
+        </CCol>
+      </CRow>
+      <CRow
+        xs={{ cols: 1 }} md={{ cols: 1 }} lg={{ cols: 1 }}
+        className="justify-content-center mt-4"
+      >
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              <h4 className="my-3">House List</h4>
+            </CCardHeader>
+            <CCardBody>
+              {!houseList || houseList.length === 0 ? (
+                <div className="text-center py-4">
+                  <h2>No bookings found.</h2>
+                </div>
+              ) : (
+                <CTable hover responsive>
+                  <CTableHead>
+                    <CTableRow>
+                      <CTableHeaderCell className="text-center">House Name</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Address</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Daily Rent</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Revenue</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Status</CTableHeaderCell>
+                      <CTableHeaderCell className="text-center">Actions</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
+                    {houseList.map((house) => (
+                      <CTableRow key={house.id} className="align-middle">
+                        <CTableDataCell className={styles['house-name']} title={house.houseName}>
+                          {house.houseName}
+                        </CTableDataCell>
+                        <CTableDataCell>{house.address}</CTableDataCell>
+                        <CTableDataCell className="text-end">
+                          <CurrencyFormat value={house.price} />
+                        </CTableDataCell>
+                        <CTableDataCell className="text-end">
+                          <CurrencyFormat value={house.totalRevenue} />
+                        </CTableDataCell>
+                        <CTableDataCell className="text-center">
+                          {house.status}
+                        </CTableDataCell>
+                        <CTableDataCell className="d-flex justify-content-center">
+                          <CButton color="warning" onClick={() => handleUpdateStatus(house)}>Update Status</CButton>
+                        </CTableDataCell>
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                </CTable>
+              )}
+              {houseList && houseList.length > 0 &&
+                <UserPagination page={page} totalPages={totalPages} setPage={setPage} />}
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+      <CModal visible={modalVisible} onClose={handleCloseModal}>
+        <CModalHeader>Update House Status</CModalHeader>
+        <CModalBody>
+          <CRow>
+            <CCol sm={12} md={5} lg={5} className="d-flex flex-column flex-grow-1">
+              <label className="fw-bolder ps-1">Status</label>
+              <CFormSelect
+                value={newStatus}
+                onChange={(e) => setNewStatus(e.target.value)}
+                className="form-control border-1 text-center bring-front"
+              >
+                <option value="AVAILABLE">AVAILABLE</option>
+                <option value="MAINTAINING">MAINTAINING</option>
+              </CFormSelect>
+            </CCol>
+            <CCol sm={12} md={7} lg={7} className="d-flex flex-column flex-grow-1">
+              {newStatus === 'MAINTAINING' &&
+                <div>
+                  <label className="fw-bolder ps-1">Date Range</label>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(dates) => {
+                      const [start, end] = dates
+                      setStartDate(start)
+                      setEndDate(end)
+                    }}
+                    className="form-control border-1 text-center bring-front"
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={minAvailableDate}
+                    maxDate={maxAvailableDate || undefined}
+                    dateFormat="dd/MM/yyyy"
+                    excludeDateIntervals={bookedDates}
+                    selectsRange
+                  />
+                </div>
+              }
+            </CCol>
+          </CRow>
+        </CModalBody>
+        <CModalFooter>
+          <CButton
+            color="primary"
+            onClick={handleSubmitUpdate}
+          >
+            Confirm
+          </CButton>
+        </CModalFooter>
+      </CModal>
+    </CContainer>
+  )
+}
+
+export default HouseListTable
--- src/redux/slices/houseSlice.js
+++ src/redux/slices/houseSlice.js
@@ -8,6 +8,8 @@ export const initialState = {
   totalPages: 0,
   loading: false,
   error: null,
+  bookedDates: [],
+  latestAvailableDate: null,
 }
 
 export const getHouseList = createAsyncThunk(
@@ -15,13 +17,12 @@ export const getHouseList = createAsyncThunk(
   async ({username,page, size},thunkAPI) => {
     try {
       const response = await api.get(`/houses/host-house-list/${username}?page=${page}&size=${size}`)
-      console.log(response.data.content)
       return {
         houseList: response.data.content,
         totalPages: response.data.totalPages,
       };
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error retrieving profile data!')
+      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error retrieving House List data!')
     }
   },
 )
@@ -39,7 +40,61 @@ export const searchHouses = createAsyncThunk(
         totalPages: response.data.totalPages,
       };
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error retrieving profile data!')
+      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error retrieving search data!')
+    }
+  },
+)
+
+export const getBookedDates = createAsyncThunk(
+  'houses/getBookedDates',
+  async ({houseId},thunkAPI) => {
+    try {
+      const response = await api.get(`/houses/${houseId}/booked-dates`)
+      console.log(response.data)
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error retrieving Booked Dates data!')
+    }
+  },
+)
+
+export const getLatestAvailableDate = createAsyncThunk(
+  'houses/getLatestAvailableDate',
+  async ({ houseId, date },thunkAPI) => {
+    try {
+      const response = await api.post(`/houses/house-edge-date`, {
+        houseId: houseId,
+        date: date,
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error retrieving Latest Available Date data!')
+    }
+  },
+)
+
+export const updateHouseStatus = createAsyncThunk(
+  'houses/updateHouseStatus',
+  async ({ houseId, status },thunkAPI) => {
+    try {
+      const response = await api.put(`/houses/${houseId}/update-status?status=${status}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error updating house status!')
+    }
+  },
+)
+
+export const creatMaintenanceRecord = createAsyncThunk(
+  'houses/creatMaintenanceRecord',
+  async ({ houseId, startDate, endDate },thunkAPI) => {
+    console.log({houseId: houseId, startDate: startDate, endDate: endDate})
+    try {
+      const response = await api.post(`/houses/create-maintenance-record`,
+        {houseId: houseId, startDate: startDate, endDate: endDate});
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data?.message || 'Error creating new Maintenance Record!')
     }
   },
 )
@@ -82,6 +137,28 @@ const houseSlice = createSlice({
       .addCase(searchHouses.rejected, (state, action) => {
         state.error = action.payload
         state.loading = false
+      })
+      .addCase(getBookedDates.fulfilled, (state, action) => {
+        state.bookedDates = action.payload
+      })
+      .addCase(getBookedDates.rejected, (state, action) => {
+        state.error = action.payload
+      })
+      .addCase(getLatestAvailableDate.fulfilled, (state, action) => {
+        state.latestAvailableDate = action.payload
+      })
+      .addCase(getLatestAvailableDate.rejected, (state, action) => {
+        state.error = action.payload
+      })
+      .addCase(updateHouseStatus.fulfilled, () => {
+      })
+      .addCase(updateHouseStatus.rejected, (state, action) => {
+        state.error = action.payload
+      })
+      .addCase(creatMaintenanceRecord.fulfilled, () => {
+      })
+      .addCase(creatMaintenanceRecord.rejected, (state, action) => {
+        state.error = action.payload
       })
   },
 });
