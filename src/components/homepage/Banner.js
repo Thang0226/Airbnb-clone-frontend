c0ec6d7d0266b540a9482bdf7136b28c7eadac40
tmtri3102 <tmtri310251@gmail.com>
2025-03-06 18:24:06 +0700

    Change banner

--- src/components/homepage/Banner.js
+++ src/components/homepage/Banner.js
@@ -1,18 +1,18 @@
-import { CCarousel, CCarouselItem, CImage } from '@coreui/react'
-import "./homepage.css"
+import { CCarousel , CCarouselItem , CImage } from '@coreui/react'
+import './homepage.css'
 
 export default function Banner() {
 
   return (
-    <CCarousel controls interval={3000} transition='slide'>
+    <CCarousel controls interval={3000} transition="slide">
       <CCarouselItem>
-        <CImage className="d-block w-100 banner" src="/hero/banner_1.png" alt="slide 1" />
+        <CImage className="d-block w-100 banner object-fit-cover" src="/hero/banner_1.png" alt="slide 1" />
       </CCarouselItem>
       <CCarouselItem>
-        <CImage className="d-block w-100 banner" src="/hero/banner_2.png" alt="slide 2" />
+        <CImage className="d-block w-100 banner object-fit-cover" src="/hero/banner_2.png" alt="slide 2" />
       </CCarouselItem>
       <CCarouselItem>
-        <CImage className="d-block w-100 banner" src="/hero/banner_3.png" alt="slide 3" />
+        <CImage className="d-block w-100 banner object-fit-cover" src="/hero/banner_3.png" alt="slide 3" />
       </CCarouselItem>
     </CCarousel>
   )
